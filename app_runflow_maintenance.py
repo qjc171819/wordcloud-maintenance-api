073498b6dc973b3e5e453778b484addabba11781
qjc171819 <46129422+qjc171819@users.noreply.github.com>
2026-02-09 13:25:23 +0800

    app_runflow_maintenance.py

--- app_runflow_maintenance.py
+++ app_runflow_maintenance.py
@@ -33,7 +33,7 @@ def generate_url(base64_image):
     api_key = '6d207e02198a847aa98d0a2a901485a5'
     # user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36 Edg/139.0.0.0'
     req = requests.post(f'{postUrl}?key={api_key}', data = {'image': base64_image}
-                        #,headers = {'user-agent': user_agent}
+                        ,headers = {'user-agent': user_agent}
                         )
     js = req.json()
     # image_url = js['data']['image']['url']
@@ -259,6 +259,7 @@ if __name__ == '__main__':
     # 生产环境应设置debug=False
 
     app.run(host='0.0.0.0', port=5090, debug=False)
+
 
 
 
